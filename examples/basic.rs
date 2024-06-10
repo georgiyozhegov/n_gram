1b0f20ce449647dcb8418476bb5cf24a4d974eb9
George <georgiyozhegov.rs@gmail.com>
2024-06-10 17:21:29 +0400

    Formatting: basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,25 +1,23 @@
 use n_gram::*;
 
-fn main() 
+fn main()
 {
-    let config = Config::default();
-    let mut model = Model::new(config);
+        let config = Config::default();
+        let mut model = Model::new(config);
 
-    // requires `corpus` feature
-    let corpus = tiny_corpus()
-        .iter()
-        .map(|t| sos(eos(tokenize(t.to_owned()))))
-        .collect::<Vec<_>>();
+        // requires `corpus` feature
+        let corpus = tiny_corpus()
+                .iter()
+                .map(|t| sos(eos(tokenize(t.to_owned()))))
+                .collect::<Vec<_>>();
 
-    model.train(corpus);
+        model.train(corpus);
 
-    let mut tokens = sos(tokenize("The quick".to_owned()));
-    let max = 10;
-    model.generate(&mut tokens, max);
+        let mut tokens = sos(tokenize("The quick".to_owned()));
+        let max = 10;
+        model.generate(&mut tokens, max);
 
-    model.save("model.json").unwrap();
-
-    model.reset();
-
-    model.load("model.json").unwrap();
+        model.save("model.json").unwrap();
+        model.reset();
+        model.load("model.json").unwrap();
 }
